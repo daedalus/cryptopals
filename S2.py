f3be0d9b73f3815f483c4fd936d93dffedd4dcfb
Darío Clavijo <daedalus2027@gmail.com>
2017-10-13 01:32:04 -0300

    CBC mode WIP

--- S2.py
+++ S2.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 # Autor Dario Clavijo 2017
+from S1 import *
+from Crypto.Cipher import AES
 
 pad = lambda s,n: s + chr(n- len(s)) * (n-len(s))
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
+def encryptECB(key,enc):
+        cipher = AES.new(key, AES.MODE_ECB)
+        return cipher.encrypt(enc)
+
+def decryptCBC(data,key,iv):
+	l = len(iv)
+	tmp = ""
+	lastblock = iv
+	for i in range(0,len(data),l):
+		block = data[i:i+l]
+		decrypted = decryptECB(block,key)
+		plain = XOR(decrypted,lastblock)
+		tmp += plain
+		lastblock = block
+	return tmp
+
 def test1():
+	print "C9-S2C1"
 	u = ("Dario Clavijo")
 	print u.encode('hex')
 	p = pad(u,20)
@@ -12,4 +31,10 @@ def test1():
 	u = unpad(p)
 	print u.encode('hex')
 
-test1()
+def test2():
+	l = AES.block_size
+	data = open('10.txt').read().replace('\n','').decode('base64')
+	print decryptCBC(data,b'YELLOW SUBMARINE','\x00' * l)
+
+#test1()
+test2()

4882b36c6efc775fd4774bb1ba44881b09066ee3
Darío Clavijo <daedalus2027@gmail.com>
2017-10-12 23:22:02 -0300

    cleanup

--- s1c1.py
+++
@@ -1,4 +0,0 @@
-i = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-o = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-
-print i.decode('hex').encode('base64').replace('\n','') == o

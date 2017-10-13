c127753d7bb0743bee28685d0820b0f46bbda68d
Dario Clavijo <daedalus2027@gmail.com>
2017-10-13 19:49:25 -0300

    challenge 12 WIP

--- S2.py
+++ S2.py
@@ -29,18 +29,23 @@ def decryptCBC(data,key,iv):
 		lastblock = block
 	return tmp
 
-def oracleECBCBC(message):
-	BS=AES.block_size
-	key = Random.new().read(BS)
+def oracleECBCBC(message,key):
+	BS = AES.block_size
 	g = Random.new().read(10)
 	r = random.randrange(2)
-	message = (g + message + g).zfill(96)
+	message = (g + message + g).zfill(128)
 	if r == 1:
 		data = encryptECB(message,key)
 	else:
 		IV = Random.new().read(BS)
 		data = encryptCBC(message,key,IV)	
 	return data
+
+def detectOracleMethod(encryptionOracle,message,key):
+	c = encryptionOracle(message,key)
+	if detectECB(c):
+		return "ECB",c.encode('hex')
+	return "CBC",c.encode('hex')
 
 def test1():
 	print "C9-S2C1"
@@ -56,18 +61,68 @@ def test2():
 	KEY = pad(b'YELLOW SUBMARINE',16)
 	print KEY,KEY.encode('hex'),len(KEY),len(IV)
 	data = b64decode(open('10.txt').read())
-	print decryptCBC(data,'YELLOW SUBMARINE',IV)
+	print decryptCBC(data,KEY,IV)
 
 
 def test3():
 	for i in range(0,10):
-		c = oracleECBCBC('THIS IS A VERY IMPORTANT MESSAGE FOR DECRYPT')
-		ecb = detectECB(c)
-		if ecb > 0:
-			print "ECB",c.encode('hex')
-		else:
-			print "CBC",c.encode('hex')
+		key = Random.new().read(16)
+		msg = 'THIS IS A VERY IMPORTANT MESSAGE FOR DECRYPT'
+		print detectOracleMethod(oracleECBCBC,msg,key) 
+
+def test4():
+	key = Random.new().read(16)
+	UNKNOWN = b64decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
+	KNOWN = ""
+
+		
+
+	def oracle(msg,key):
+		c = encryptECB(msg,key)
+		return c # ECB detected
+
+	if detectECB(oracle(UNKNOWN.zfill(224),key)) == 0:
+		sys.exit(0)
+	else:
+		print "ECB MODE detected"
+
+	def detectOracleBLOCKSIZE(oracle,msg,key):
+		c = oracle(msg,key)
+		B,s = findBLOCKSize(1,c)
+		return B
+
+	print "LEN UNKNOWN DATA:",len(UNKNOWN)
+	BLOCKSIZE = detectOracleBLOCKSIZE(oracle,UNKNOWN.zfill(256),key)
+
+	print "BLOCKSIZE detected:",BLOCKSIZE
+
+
+
+	def findnNextByte(oracle,BLOCKSIZE,KNOWN):	
+		#print len(UNKNOWN[0:BLOCKSIZE])
+		target = oracle(UNKNOWN[0:BLOCKSIZE],key)
+		#print target
+		print "Target:",target.encode('hex')
+		ISTR = UNKNOWN[0:BLOCKSIZE-1]
+		for i in range(0,255):
+			test = ISTR + chr(i)
+			testc = oracle(test,key)
+			if testc == target:
+				return chr(i)
+		return None
+
+	while True:
+		b = findnNextByte(oracle,BLOCKSIZE,UNKNOWN)
+		KNOWN += b
+		if b != None:
+			break
+
+	print KNOWN
+		
+	
 
 #test1()
 #test2()
-test3()
+#test3()
+test4()
+

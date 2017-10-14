17155c4b16335bb39adb3b26a2ca3501d6158c43
Darío Clavijo <daedalus2027@gmail.com>
2017-10-14 20:17:47 -0300

    code refactory

--- S2.py
+++ S2.py
@@ -47,6 +47,49 @@ def detectOracleMethod(encryptionOracle,message,key):
 		return "ECB",c.encode('hex')
 	return "CBC",c.encode('hex')
 
+def findECBBLOCKSize(data):
+        BESTSCORE = sys.float_info.max
+        KEYSIZE=0
+        for keysize in range(2,64):
+                blocks = []
+                for j in range(0,len(data),keysize):
+			b = data[j:j+keysize]
+			if len(b) == keysize:
+                        	blocks.append(data[j:j+keysize])
+                pairs = list(itertools.combinations(blocks, 2))
+                scores = [hammingDistance(p[0], p[1])/float(keysize) for p in pairs]
+		if len(scores) > 1:
+                	s = float(sum(scores) / len(scores))
+                	if s < BESTSCORE:
+                	        BESTSCORE = s
+                        	KEYSIZE = keysize
+        return KEYSIZE,BESTSCORE
+
+def detectOracleBLOCKSIZE(oracle,msg):
+	return findECBBLOCKSize(oracle(msg))[0]
+
+def getNextByte(oracle,blockSize,knownString):
+	myString = "0" * (blockSize - (len(knownString) % blockSize) -1 )
+	d = {}
+	for i in range(0,256):
+		candidate = oracle(myString+knownString+chr(i))	
+		l = len(myString) + len(knownString)+1
+		d[candidate[0:l]] = i
+	target = oracle(myString)
+	u = target[0:l]
+	if u in d:
+		return d[u]
+	return None
+
+def crackECBOracle(oracle,blockSize,knownBytes):
+	while True:
+		b = getNextByte(oracle,blockSize,knownBytes)
+		if b is None:
+			break
+		knownBytes += chr(b)
+
+	return knownBytes
+
 def test1():
 	print "C9-S2C1"
 	u = ("Dario Clavijo")
@@ -63,7 +106,6 @@ def test2():
 	data = b64decode(open('10.txt').read())
 	print decryptCBC(data,KEY,IV)
 
-
 def test3():
 	for i in range(0,10):
 		key = Random.new().read(16)
@@ -73,18 +115,12 @@ def test3():
 def test4():
 	key = Random.new().read(16)
 	UNKNOWN = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
-	KNOWN = ""
 
 	def oracle(plaintext):
 		# key is supposed to be internal to the oracle, we dont know it.
 		plaintext = pad(plaintext+b64decode(UNKNOWN),16)
 		c = encryptECB(plaintext,key)
 		return c # ECB detected
-
-	def detectOracleBLOCKSIZE(oracle,msg):
-		c = oracle(msg)
-		B,s = findBLOCKSize(1,c)
-		return B
 
 	BLOCKSIZE = detectOracleBLOCKSIZE(oracle,"0"*32)
 
@@ -96,29 +132,26 @@ def test4():
 	else:
 		print "ECB MODE detected"
 
-	def getNextByte(oracle,blockSize,knownString):
-		myString = "0" * (blockSize - (len(knownString) % blockSize) -1 )
-		d = {}
-		for i in range(0,256):
-			candidate = oracle(myString+knownString+chr(i))	
-			l = len(myString) + len(knownString)+1
-			d[candidate[0:l]] = i
-		target = oracle(myString)
-		u = target[0:l]
-		if u in d:
-			return d[u]
-		return None
+	print crackECBOracle(oracle,BLOCKSIZE,"")
 
-	while True:
-		b = getNextByte(oracle,BLOCKSIZE,KNOWN)
-		if b is None:
-			break
-		KNOWN += chr(b)
+def KVtoJSON(s):
+	myjson = "{\n"
+	for kv in s.split("&"):
+		k,v = kv.split("=")
+		myjson += k + ":" + v + "\n"
+	myjson += "}"
+	return myjson
 
-	print KNOWN
+def JSONtoKV(s):
+	s = replace("{\n",'').replace("}\n","")
+	s = s.split("\n")
+	kvs = []
+	for l in s:
+		k,v = l.split(':')
+		kvs.append(k + "=" + v)
+	return "&".join(kvs)
 
 #test1()
 #test2()
 #test3()
 test4()
-
